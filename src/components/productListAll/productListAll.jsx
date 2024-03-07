dc2a644d0ff2a066dd3f645e78669784492ca750
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-03-06 22:51:54 -0300

    product list

--- src/components/productListAll/productListAll.jsx
+++ src/components/productListAll/productListAll.jsx
@@ -4,28 +4,16 @@ import Product from "../product/product";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ProductListAll = ({ listTitle, selectedProducts }) => {
-  const colClass = "col-lg-3 col-md-4 col-sm-6 col-12";
-  const textClass = "productList-Text";
-
-  const productsInGroups = [];
-  const groupSize = 4;
-
-  for (let i = 0; i < selectedProducts.length; i += groupSize) {
-    productsInGroups.push(selectedProducts.slice(i, i + groupSize));
-  }
-
   return (
-    <div className="full-list">
-      <label className={textClass}>{listTitle}</label>
-      {productsInGroups.map((group, groupIndex) => (
-        <div key={groupIndex} className="row mb-3">
-          {group.map((product, index) => (
-            <div key={index} className={`mb-3 ${colClass} text-center`}>
-              <Product productInfo={product} />
-            </div>
-          ))}
-        </div>
-      ))}
+    <div className="full-list-all">
+      <label className="productList-Text">{listTitle}</label>
+      <div className="row mb-3 col-6">
+        {selectedProducts.map((product, index) => (
+          <div key={index} className="col mb-3">
+            <Product productInfo={product} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
