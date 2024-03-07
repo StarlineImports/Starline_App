41cf41517ad302d2b365a33e04383cf83afce028
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-03-07 20:17:35 -0300

    ball page

---
+++ src/pages/productListPageBall copy/productListPageBall.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import HeaderComponent from "../../components/Header/HeaderComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useParams } from "react-router-dom";
+
+import Footer from "../../components/footer/footer";
+import ProductListAll from "../../components/productListAll/productListAll";
+
+import ProductData from "../../product.json";
+import "./productListPageBall.css";
+
+const ProductListPageBall = () => {
+  const { subcategory } = useParams();
+
+  const footballProducts = ProductData.filter(
+    (product) => product.subcategory === "ball"
+  );
+
+  const [orderBy, setOrderBy] = useState("popularity");
+
+  const handleOrderByChange = (e) => {
+    setOrderBy(e.target.value);
+  };
+
+  const sortProducts = (products, orderBy) => {
+    switch (orderBy) {
+      case "popularity":
+        return products.sort((a, b) => b.popularity - a.popularity);
+      case "newest":
+        return products.sort((a, b) => new Date(b.date) - new Date(a.date));
+      case "priceHigh":
+        return products.sort((a, b) => b.price - a.price);
+      case "priceLow":
+        return products.sort((a, b) => a.price - b.price);
+      default:
+        return products;
+    }
+  };
+
+  const sortedProducts = sortProducts(footballProducts, orderBy);
+
+  return (
+    <>
+      <div className="product-list-page-container">
+        <div className="product-list-page-header">
+          <HeaderComponent />
+        </div>
+        <div className="product-list-page-body">
+          <div className="filter-container"></div>
+          <div className="products-header">
+            <h5>Bolas</h5>
+            <div className="order-filter-container">
+              <label className="order-filter-label">Ordenar Por</label>
+              <select
+                className="order-filter"
+                value={orderBy}
+                onChange={handleOrderByChange}
+              >
+                <option value="popularity">Mais Populares</option>
+                <option value="newest">Lançamentos</option>
+                <option value="priceHigh">Maior Preço</option>
+                <option value="priceLow">Menor Preço</option>
+              </select>
+            </div>
+          </div>
+          <ProductListAll selectedProducts={sortedProducts} />
+        </div>
+        <div className="product-list-page-footer">
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductListPageNFL;
