13099bf8165b3d93c1013790fb881b03b88dad80
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-03-07 20:27:04 -0300

    basket correção css

--- src/pages/productListPageBasket/productListPageBasket.jsx
+++ src/pages/productListPageBasket/productListPageBasket.jsx
@@ -7,7 +7,7 @@ import Footer from "../../components/footer/footer";
 import ProductListAll from "../../components/productListAll/productListAll";
 
 import ProductData from "../../product.json";
-import "./productListPageAccessories.css";
+import "./productListPageBasket.css";
 
 const ProductListPageBasket = () => {
   const { subcategory } = useParams();
