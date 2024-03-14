993dd3478d01a1dc2a608269531e7603af7c9652
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-03-13 21:15:59 -0300

    botao

--- src/components/register/step1/registerComponent1.jsx
+++ src/components/register/step1/registerComponent1.jsx
@@ -81,7 +81,7 @@ const RegisterComponent1 = () => {
             className="form-control button"
             id="inputPassword"
           >
-            Criar
+            Cria
           </button>
         </div>
       </form>
