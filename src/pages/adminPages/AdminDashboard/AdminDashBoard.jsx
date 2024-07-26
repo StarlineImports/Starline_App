109040d9c01ff747a6f0ec93458790fb9a318f48
geilsonfreire <geilsonfreireleite@gmail.com>
2024-07-26 17:07:40 -0300

    Foi instador a biblioteca react-apexcharts e foi criado o 1º grafico do DashBoard e ciado a seçao detalhes dos pedidos.

--- src/pages/adminPages/AdminDashboard/AdminDashBoard.jsx
+++ src/pages/adminPages/AdminDashboard/AdminDashBoard.jsx
@@ -4,13 +4,17 @@ import React from 'react'
 // Imports Css
 import './AdminDashBoard.css'
 
+// Imports Componenets
+import Admincharts from '../../../components/AdminComponents/AdminCharts/AdminApexcharts'
+
 // imports icons and images
 import { BsGraphUpArrow } from "react-icons/bs";
 import { 
     MdGroups,
     MdOutlineTrendingUp,
     MdChecklist,
-    MdHistory
+    MdHistory,
+    MdFilterAlt
 } from "react-icons/md";
 
 const AdminDashBoard = () => {
@@ -65,8 +69,61 @@ const AdminDashBoard = () => {
                             <span className='text-update'>Dados mais recentes</span>
                         </div>
                     </div>
-                    
+                </section>
 
+                <section className='Apexcharts'>
+                    <div className="container-charts">
+                        <Admincharts /> 
+                    </div>
+                </section>
+
+                <section className='detalhes-pedidos'>
+                    <div className="subtitle">
+                        <h2>Detalhes dos pedidos</h2>
+                        <form>
+                            <label for="months"><i><MdFilterAlt /></i></label>
+                            <select id="months" name="months">
+                                <option value="janeiro">Janeiro</option>
+                                <option value="fevereiro">Fevereiro</option>
+                                <option value="marco">Março</option>
+                                <option value="abril">Abril</option>
+                                <option value="maio">Maio</option>
+                                <option value="junho">Junho</option>
+                                <option value="julho">Julho</option>
+                                <option value="agosto">Agosto</option>
+                                <option value="setembro">Setembro</option>
+                                <option value="outubro">Outubro</option>
+                                <option value="novembro">Novembro</option>
+                                <option value="dezembro">Dezembro</option>
+                            </select>
+                        </form>
+                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>IMG</th>
+                                <th>Produto</th>
+                                <th>Local</th>
+                                <th>Data</th>
+                                <th>Quantidade</th>
+                                <th>Valor-Unitario</th>
+                                <th>Valor-Total</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><img src="https://cambuci.vtexassets.com/arquivos/ids/1033160/Bola-de-Futsal-Penalty-S11-R1-XXIV.jpg?v=638372870109700000" alt="Bola Penalty"/></td>
+                                <td>Produto 1</td>
+                                <td>Local 1</td>
+                                <td>01/01/2021</td>
+                                <td>1</td>
+                                <td>R$ 100,00</td>
+                                <td>R$ 100,00</td>
+                                <td>Entregue</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </section>
 
             </main>
